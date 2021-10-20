d2bc8c8dd6b3bc49567123f5f18e3048fd8acb02
Filip Koza <peacefilip1989@gmail.com>
2021-10-20 05:06:08 +0200

    thumbs

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,10 @@
+// import { useState } from 'react'
 import Button from '@mui/material/Button'
+import likeThumb from '../assets/like.png'
+import dislikeThumb from '../assets/dislike.png'
 
 const Modal = ({ plot, actors, rating, writer, setInfo }) => {
+  //   const [thumb, setThumb] = useState(null)
   return (
     <div aria-label="Modal window" className="modal_main">
       <div className="modal_back" aria-label="Background of modal window"></div>
@@ -8,7 +12,7 @@ const Modal = ({ plot, actors, rating, writer, setInfo }) => {
         <Button
           type="button"
           className="close_button"
-          variant="filled"
+          variant="contained"
           onClick={() => {
             setInfo(previousValue => ({ ...previousValue, open: false }))
             document.body.style.overflow = 'scroll'
@@ -17,9 +21,22 @@ const Modal = ({ plot, actors, rating, writer, setInfo }) => {
           X
         </Button>
         <li className="characteristics">{plot}</li>
-        <li className="characteristics">Actors:{actors}</li>
-        <li className="characteristics">Writer: {writer}</li>
-        <li className="characteristics">Rating:{rating}</li>
+        <li className="characteristics">
+          <span>Actors: </span>
+          {actors}
+        </li>
+        <li className="characteristics">
+          <span>Writer: </span> {writer}
+        </li>
+        <li className="characteristics">
+          <span>Rating: </span>
+          {rating}{' '}
+          {rating < 7 ? (
+            <img src={dislikeThumb} alt="Dislike" />
+          ) : (
+            <img src={likeThumb} alt="Like" />
+          )}
+        </li>
       </ul>
     </div>
   )
